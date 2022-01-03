d7b1346cf670651ff5f6fdb76fd2a90f8f7edfc1
Stepan Riha <github@nonplus.net>
2022-01-03 00:30:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def move(current_row, current_col, direction):
+  # Some code
+  return [ current_row, current_col ]
+
+[ player_row, player_col ] = move(1, 2, 'up')
+
+print(f'New row is {player_row}')
+print(f'New col is {player_col}')
